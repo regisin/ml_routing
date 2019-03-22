2927d1aa7dce88d82efc4e4675fb3940e319a235
Paulo Alexandre Regis <regisin@gmail.com>
2019-03-22 10:33:48 -0700

    placeholders

---
+++ classes.py
@@ -0,0 +1,6 @@
+class Node():
+    pass
+
+class Network():
+    def dijkstra(self, source):
+        pass
---
+++ utils.py
@@ -0,0 +1,34 @@
+
+def compute_link_weight_delay(graph):
+    """
+    This function will update the edges of the graph based on delay metric
+    """
+    pass
+
+
+def compute_link_weight_energy(graph):
+    """
+    This function will update the edges of the graph based on energy metric
+    """
+    pass
+
+
+def compute_link_weight_split(graph):
+    """
+    This function will update the edges of the graph based on split (icnc) metric
+    """
+    pass
+
+
+def compute_link_weight_hop(graph):
+    """
+    This function will update the edges of the graph based on hop count metric
+    """
+    pass
+
+def get_state_from_source(graph, source=0, hops=1):
+    """
+    This will basically return the ground truth data for the ML algo.
+    Hops = the radius from source node to gather the data
+    """
+    pass
